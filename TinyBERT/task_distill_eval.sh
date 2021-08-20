72293ab4cda917b6fc071a233fe9fa1ca9aa880a
mcao610 <c_meng@outlook.com>
2021-08-20 09:09:24 -0700

    update bash files & .gitignore

--- TinyBERT/task_distill_eval.sh
+++ TinyBERT/task_distill_eval.sh
@@ -2,7 +2,7 @@
 source ~/env37/bin/activate
 
 TASK_NAME=MNLI
-TINYBERT_DIR=/home/mcao610/scratch/TinyBERT_TEST/${TASK_NAME}/final-similarity
+TINYBERT_DIR=/home/mcao610/scratch/TinyBERT_TEST/${TASK_NAME}/final-similarity-10
 TASK_DIR=/home/mcao610/scratch/glue_data/${TASK_NAME}
 OUTPUT_DIR=/home/mcao610/scratch/TinyBERT_TEST/${TASK_NAME}/eval  # output directory
 
--- TinyBERT/task_distill_pred.sh
+++ TinyBERT/task_distill_pred.sh
@@ -3,12 +3,12 @@ source ~/env37/bin/activate
 
 TASK_NAME=MNLI
 FT_BERT_BASE_DIR=/home/mcao610/scratch/huggingface/MNLI/uncased/
-TMP_TINYBERT_DIR=/home/mcao610/scratch/TinyBERT_TEST/MNLI/similarity
+TMP_TINYBERT_DIR=/home/mcao610/scratch/TinyBERT_TEST/MNLI/similarity-256
 TASK_DIR=/home/mcao610/scratch/glue_data/${TASK_NAME}
-TINYBERT_DIR=/home/mcao610/scratch/TinyBERT_TEST/${TASK_NAME}/test  # output directory
+TINYBERT_DIR=/home/mcao610/scratch/TinyBERT_TEST/${TASK_NAME}/final-similarity-256  # output directory
 
 mkdir $TINYBERT_DIR
-python task_distill.py --pred_distill \
+python /home/mcao610/Pretrained-Language-Model/TinyBERT/task_distill.py --pred_distill \
                        --teacher_model ${FT_BERT_BASE_DIR} \
                        --student_model ${TMP_TINYBERT_DIR} \
                        --data_dir ${TASK_DIR} \
@@ -18,6 +18,6 @@ python task_distill.py --pred_distill \
                        --do_lower_case \
                        --learning_rate 3e-5 \
                        --num_train_epochs 3 \
-                       --eval_step 300 \
+                       --eval_step 1000 \
                        --max_seq_length 128 \
                        --train_batch_size 32;
